621b7b592d77e30b539ad08f66c08b49d453373b
Alec Chen <cca169@sfu.ca>
2015-10-03 21:14:49 -0700

    add command_preprocessor header file

---
+++ include/Command_Parser.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <boost/tokenizer.hpp>
+
+/*include "player.h"*/
+
+/*
+validate user input, and then passed the command to game model.
+return actual command;
+ */
+
+class Command_Parser{
+public:
+    Command_Parser();
+    ~Command_Parser();
+    void tokenize_input(std::string in);
+    bool is_Validated(std::vector<std::string>);
+
+prvivate:
+    std::vector<std::string> input_tokens;
+    enum Command_Set{ MOVE, KILL, USE };
+    /*may need to search for a file that lists of  */
+    bool lookup_Command_Set(std::string cmd);
+   /* lookup_User_Command_Alias(std::String cmd, Player id);*/
+
+};
