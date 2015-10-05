0663e8b5ca053a12ed8503dae20a9b03002ec888
cca169 <cca169@sfu.ca>
2015-10-05 02:44:51 -0700

    tested tokenizing input

--- include/Command_Parser.h
+++ include/Command_Parser.h
@@ -12,16 +12,16 @@ return validated command;
 
 class Command_Parser{
 public:
-    Command_Parser();
-    ~Command_Parser();
+    //Command_Parser();
+    //~Command_Parser();
     void tokenize_input(std::string in);
-    bool is_Validated(std::vector<std::string>);
+    //bool is_Validated(std::vector<std::string>);
 
 prvivate:
-    std::vector<std::string> input_tokens;
-    enum Command_Set{ MOVE, KILL, USE };
+    //std::vector<std::string> input_tokens;
+    //enum Command_Set{ MOVE, KILL, USE };
     /*may need to search for a file that lists of  */
-    bool lookup_Command_Set(std::string cmd);
+   // bool lookup_Command_Set(std::string cmd);
    /* lookup_User_Command_Alias(std::String cmd, Player id);*/
 
 };
