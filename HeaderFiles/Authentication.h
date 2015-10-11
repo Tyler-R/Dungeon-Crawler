0896eabe87af463b591d7a83cc6aebb3a4efc5bc
Arsalan Noroozzadeh <anoroozz@sfu.ca>
2015-10-11 13:03:50 -0700

    Added the public interface for Authentication

---
+++ HeaderFiles/Authentication.h
@@ -0,0 +1,24 @@
+
+
+
+#ifndef __Authentication__Authentication__
+#define __Authentication__Authentication__
+
+#include <stdio.h>
+#include <string>
+#include "UserLogin.h"
+#include "UserSignUp.h"
+
+class Authentication {
+    std::string userName;
+    std::string userPassword;
+    
+public:
+    Authentication(std::string userName, std::string userPassword);
+    bool login();
+    bool logout();
+    bool signUp();
+    
+};
+
+#endif
