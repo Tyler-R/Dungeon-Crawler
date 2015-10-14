95940477ee16e45e8ca47a8f633e9d9afbaf0809
cca169 <cca169@sfu.ca>
2015-10-13 17:03:34 -0700

    making it cmake compatible

---
+++ lib/world/creature.cpp
@@ -0,0 +1,95 @@
+//
+//  creature.cpp
+//  TeamProject_cmpt373
+//
+//  Created by JasonYang on 2015-10-03.
+//  Copyright © 2015 JasonYang. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "creature.h"
+
+// constructor
+Creature::Creature() {
+    // srand(static_cast<unsigned int>(time(0)));
+    
+    setName();
+    setHP();
+    setAlive();
+    setDes();
+}
+
+// destructor
+//Creature::~Creature() {
+//    delete creatureName;
+//    delete creatureHP;
+//    delete isAlive;
+//}
+
+
+// set name
+void Creature::setName() {
+    std::string totalName[] = {"brainy", "handy", "sleepy", "hefty", "grumpy"};
+    int randName = rand() % (5);
+    creatureName = totalName[randName];
+}
+
+// set description
+void Creature::setDes() {
+    if(creatureName.compare("brainy") == 0) {
+        creatureDes = "Brainy Smurf is here, numbling something to himself.";
+    }else if (creatureName.compare("handy") == 0) {
+        creatureDes = "Handy Smurf is building something here.";
+    }else if (creatureName.compare("sleepy") == 0) {
+        creatureDes = "Sleepy Smurf is sleep walking here.";
+    }else if (creatureName.compare("hefty") == 0) {
+        creatureDes = "Hefty Smurf is lifting weights here.";
+    }else { // creatureName == "grumpy"
+        creatureDes = "Grumpy Smurf is here, swearing.";
+    }
+}
+
+// set HP
+void Creature::setHP() {
+    int randNum = rand() % (5) + 1;
+    creatureHP = randNum;
+}
+
+// set isAlive
+void Creature::setAlive() {
+    isAlive = true;
+}
+
+
+// get name
+string Creature::getName() const{
+    return creatureName;
+}
+
+// get HP
+int Creature::getHP() const{
+    return creatureHP;
+}
+
+// get isAlive
+bool Creature::getAlive() const{
+    return isAlive;
+}
+// get description
+string Creature::getDes() const {
+    return creatureDes;
+}
+
+// Helper Function - Check Creature's attributes
+void Creature::checkCreature() const {
+    std::cout << "Creature check function \n";
+    std::cout << "the type is                   " << getName();
+    std::cout << "\n";
+    std::cout << "the HP is                     " << getHP();
+    std::cout << "\n";
+    std::cout << "is live(1) or dead(0)?        " << getAlive();
+    std::cout << "\n";
+    std::cout << "Description: \n" << getDes();
+    std::cout << "\n";
+}
---
+++ lib/world/world.cpp
@@ -0,0 +1,78 @@
+/*World CLASS
+
+This is a game class that contains attributes and methods for each room contained in the game world.
+
+Created By: Sarah Kim Dao
+*/
+
+#include "world.h"
+
+World::World(){ 
+	Room* lobby = new Room("Lobby","This is a lobby","The lobby is cool and clean");
+	Room* bathroom = new Room("Bathroom","This is a bathroom","The bathroom has heated floors.");
+	Room* kitchen = new Room("Kitchen","This is a kitchen","The kitchen is sparkling.");
+	Room* porch = new Room("Porch","This is a porch","The porch is a little worn.");
+	Room* study = new Room("Study","This is a study","The study is tidy and full of books.");
+	Room* bedroom = new Room("Bedroom","This is a bedroom","The bedroom is brightly lit.");
+	Room* basement = new Room("Basement","This is a basement","The basement is dark and damp.");
+
+	(*lobby).setNorth(bathroom,"The North Door leads to the Bathroom \n hint: enter\'move north\' ");
+	(*lobby).setSouth(kitchen,"The South Door leads to the Kitchen \n hint: enter\'move south\'");
+	(*lobby).setEast(porch,"The East Door leads to the Porch \n hint: enter\'move north\'");
+	(*lobby).setWest(study,"The West Door leads to the Study");
+	(*lobby).setUp(bedroom,"Upstairs leads to the Bedroom");
+	(*lobby).setDown(basement,"Downstairs leads to the Basement");
+
+	(*bathroom).setSouth(lobby,"The South Door leads to the Lobby");
+	(*kitchen).setNorth(lobby,"The North Door leads to the Lobby");
+	(*porch).setWest(lobby,"The West Door leads to the Lobby");
+	(*study).setEast(lobby,"The East Door leads to the Lobby");
+	(*bedroom).setDown(lobby,"Downstairs leads to the Lobby");
+	(*basement).setUp(lobby,"Upstairs leads to the Lobby");
+
+	roomList.push_back(lobby); 
+	roomList.push_back(bathroom);	
+	roomList.push_back(kitchen);	
+	roomList.push_back(porch);
+	roomList.push_back(study);
+	roomList.push_back(bedroom);
+	roomList.push_back(basement);	 
+	
+	currentRoom = lobby;
+
+	NPC* monster = new NPC("monster","id:111");
+	NPC* creature = new NPC("creature","id:222");
+
+	//cout << (*monster).getName() << endl;
+	//cout << (*creature).getName() << endl;
+	//cout << (*monster).getDes() << endl;
+	//cout << (*creature).getDes() << endl;
+
+	(*lobby).getNPCs().push_back(*monster);
+
+	(*lobby).getNPCs().push_back(*creature);
+
+	//string monstername=(*lobby).getNPCs().at[0].getName();
+	
+}
+
+World::~World(){
+	for (int i = 0; i<(roomList.size()); i++){
+		delete roomList.at(i);
+	}
+}
+
+vector<Room*> World::getRoomList(){
+	return roomList;
+}
+
+
+Room* World::getRoom(int i){
+	return roomList.at(i);
+}
+
+Room* World::getCurrentRoom(){
+	return currentRoom;
+}
+
+
