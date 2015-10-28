addf7f7ab5dce74db8ceb11647750766b1b78071
Sarah Kim <skd8@sfu.ca>
2015-10-27 23:35:01 -0700

    update room.cpp

--- lib/world/room.cpp
+++ lib/world/room.cpp
@@ -1,181 +1,109 @@
-/*Room CLASS
-
-This is a game class that contains attributes and methods for each room contained in the game world.
-
+/*Header file for Room.cpp
 Created By: Sarah Kim Dao
 */
 
-#include "room.h"
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <memory>
 
-Room::Room(){
-	id = "no_id";
-	name = "no_name";
-	desc = "no_desc";
-	extDesc = "no_extDesc";
-}
+using namespace std;
 
-Room::Room(string inputId, string inputName, string inputDesc, string inputExtDesc){
-	id = inputId;
-	name = inputName;
-	desc = inputDesc;
-	extDesc = inputExtDesc;
-}
+class Door;
 
-Room::Room(Room &obj){
-	id = obj.getId();
-	name = obj.getName();
-	desc = obj.getDesc();
-	extDesc = obj.getExtDesc();
+class Room {
+	private:
+		string id;
+		string name;
+		string desc;  	  	   	
+		string extDesc;
 
-}
+		vector<string> keywordList;
 
-Room::~Room(){
+		vector<Door*> doorList;
+		//vector<NPC> npcList;
+		//vector<User> userList;
+			
+	public: 
 
-}
+		Room();
+		Room(Room &obj);
 
-string Room::getId(){
-	return id;
-}
+		Room(string inputId, string inputName, string inputDesc, string inputExtDesc); 
 
-string Room::getName(){
-	return name;
-}
+		~Room();
+ 
+		string getId();
+		string getName();
+		string getDesc();  
+		string getExtDesc();
 
-string Room::getDesc(){
-	return desc;
-}
+		vector<string> getKeywords();
 
-string Room::getExtDesc(){
-	return extDesc;
-}
+		//vector<NPC*> getNPCs();
+		//vector<User> getUsers();	
 
-
-void Room::setId(string s){
-	id = s;
-}
-
-void Room::setName(string s){
-	name = s;
-}
-
-void Room::setDesc(string s){
-	desc = s;
-}
-void Room::setExtDesc(string s){
-	extDesc = s;
-}
-
-
-void Room::addKeyword(string s){
-	keywordList.push_back(s);
-}
-
-void Room::removeKeyword(string s){
-	//Removes Keyword from KeywordList
-}
-string Room::findKeyword(string s){
-	if(find(keywordList.begin(), keywordList.end(), s) != keywordList.end()){
-		return "Keyword found!";
-	}
-		return "Keyword not found!";
-	
-}
-void Room::printKeywords(){ //To be used by the Room's Test Module only!
-	for (auto & keyword : keywordList) {
-	    cout << keyword << endl;
-	}
-}
-
-
-//LookAround
-
-vector<string> Room::getDoorList(){
-	/*
-	vector<string> doorList;
-
-	doorList.push_back(lookNorth());
-	doorList.push_back(lookSouth());
-	doorList.push_back(lookEast());
-	doorList.push_back(lookWest());
-	doorList.push_back(lookUp());
-	doorList.push_back(lookDown());
-
-	return doorList;
-	*/
-}
-
-
-vector<string> Room::getObjList(){
-
-	vector<string> objList;
-
-	return objList;
-}
-
-string Room::lookAround(){
-
-		/*
-		string objRoom = "";
-		for(auto &str: npcList){
-			objRoom += str->getName() + " ";
-		}
-		objRoom = "there are " + objRoom + "in the room"+ '\n';
-
-		return getExtDesc()+ '\n' + objRoom;
-		*/
-	}
-
-string Room::getObjDesc(string objName){
-	/*
-	for (int i=0; i<doorList.size(); i++){
-
-		//cout << "Matching " << objList[i]->getName() << " with " << objName << endl;
-		if ( doorList[i]->getName() == objName ){
-			//cout << "Match Found!" << endl;
-			return doorList[i]->getDesc();
-		}
-	}
-	for (auto &npc : npcList){
-		if(objName.compare(npc->getName())==0){
-			return npc->getDes();
-		}
-	}
-
-	return "\""+objName+"\""+" not found!\n";
-	*/
-}
-
-
-void Room::addDoor(string inputDir, string inputDesc, Room &inputRoom){
-	shared_ptr<Door> newDoor;
-	//newDoor.setDir = inputDir;
-	//newDoor.setLeadsTo() = inputRoom;
-}
-
-Door* Room::findDoor(string inputDir){
-	for (auto & door : doorList) {
-	    if (inputDir.compare(door->getDir())==0){
-		return door;
-	    }
-	}
-	return NULL;
-}
-
-
-
-
-
-
-
-
-/*
-void Room::createNPC(){
 		
-		NPC* monster = new NPC("monster","id:111");
-		NPC* creature = new NPC("creature","id:222");
+		void setId(string s);
+		void setName(string s);
+		void setDesc(string s);
+		void setExtDesc(string s);
 
-		npcList.push_back(monster);
-		npcList.push_back(creature);
-}
-*/
+		void addKeyword(string s);
+		void removeKeyword(string s);
+		string findKeyword(string s);
+		void printKeywords(); //To be used by the Room's Test Module only!
+
+		void moveTo(string dir);
+
+		vector<string> getDoorList();	
+		vector<string> getObjList();
+	
+		string lookAround();
+		string getObjDesc(string objName);
+
+		//void addNPC();
+		void addDoor(string inputDir, string inputDesc, Room &inputRoom);
+		Door* findDoor(string inputDir);
+};
+
+
+
+
+class Door {
+	private:
+		string dir;
+		string desc;  	  	   	
+		Room* leadsTo;
+
+		vector<string> keywordList;
+			
+	public: 
+	
+		Door();
+
+		Door(string inputDir, string inputDesc);
+
+		Door(Door &obj);
+
+		~Door();
+
+		void setDir(string s);
+		void setDesc(string s);
+		void setLeadsTo(Room &r);
+
+		string getDir();
+		string getDesc();
+
+		vector<string> getKeywords();
+
+		Room* getLeadsTo();
+		void addKeyword(string s);
+		void removeKeyword(string s);
+		string findKeyword(string s);
+
+		void printKeywords(); //To be used by the Door's Test Module only!
+
+};
