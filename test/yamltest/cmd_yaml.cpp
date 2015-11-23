8bfe334daf7d3855116441b224b91f675663a38f
Alec Chen <cca169@sfu.ca>
2015-11-23 14:58:20 -0800

    able to parse alias cmds in yaml

--- test/yamltest/cmd_yaml.cpp
+++ test/yamltest/cmd_yaml.cpp
@@ -3,18 +3,22 @@
 #include <string>
 #include <vector>
 #include "yaml-cpp/yaml.h"
-using namespace std;
 
 
 int main()
 {
-
-    YAML::Node commands = YAML::LoadFile("Commands.yaml");
-
-
+    std::string wordCommand ="";
+    YAML::Node commands = YAML::LoadFile("command/Commands.yaml");
+    // YAML::Value root = YAML::Parse("command/Commands.yaml");
+    // cout << root;
     // vector<string> moveAlias;
     if(commands["move"]){
-        cout<<"1"<<endl;
+      int numberWords = commands["move"].size();
+      // std:: cout << numberWords << std::endl;
+      for(int i = 0; i < numberWords; i++ ){
+        std::cout<<commands["move"][i].as<std::string>()<<std::endl;
+      }
+
     }
     // YAML::Node moveCmd = commands["move"]["alias"];
     // for (int i = 0; i < moveCmd.size(); ++i) {
