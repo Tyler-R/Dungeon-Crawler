ed693509f3c9b88911306e16e7e9d2e4316f8649
Tyler-R <trubenui@sfu.ca>
2015-11-28 18:44:03 -0800

    Add new random health formula to facilitate a formula that relies on health

--- lib/Formula/Formula.cpp
+++ lib/Formula/Formula.cpp
@@ -3,7 +3,7 @@
 Formula::Formula( int numberOfDice, int maxValueOfDice, float levelMultiplier, int constant ) 
 		: dice( numberOfDice, maxValueOfDice ), levelMultiplier(levelMultiplier), constant(constant) 
 {
-
+	
 }
 
 Formula::~Formula() {
